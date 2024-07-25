e011a7cf8f3eba3358d09716393fe8af781e0bd2
wcbing <cbing2002@outlook.com>
2024-07-25 18:37:42 +0800

    first commit

---
+++ update_gen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# check for updates
+. .venv/bin/activate
+find get -maxdepth 1 -type f -name "*.py" -exec python3 {} \;
+
+# generate the files
+cd deb
+dpkg-scanpackages -m . > Packages
+apt-ftparchive release . > Release
+gpg --yes --armor --detach-sign --sign -o Release.gpg Release
+gpg --yes --clearsign -o InRelease Release
+
+echo done
