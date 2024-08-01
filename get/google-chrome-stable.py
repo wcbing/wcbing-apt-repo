dabc015d1c9c1f396cfa105d23644d3be350e9fe
wcbing <cbing2002@outlook.com>
2024-08-01 20:06:22 +0800

    add google-chrome-stable

---
+++ get/google-chrome-stable.py
@@ -0,0 +1,12 @@
+import requests
+import json
+from module.check import check_download
+
+version_url = "https://versionhistory.googleapis.com/v1/chrome/platforms/linux/channels/stable/versions"
+res = requests.get(version_url)
+version = json.loads(res.text)["versions"][0]["version"]
+# print(version)
+
+x64_deb_url = "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+
+check_download("google-chrome-stable", version, x64_deb_url)
