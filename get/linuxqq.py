c99c1a41af7f001ef071476944a56ebb9448abda
wcbing <cbing2002@outlook.com>
2024-08-11 22:28:21 +0800

    update linuxqq

--- get/linuxqq.py
+++ get/linuxqq.py
@@ -3,10 +3,12 @@ import re
 import json
 from module.check import check_download
 
-res = requests.get("https://qq-web.cdn-go.cn/im.qq.com_new/f2ff7664/rainbow/linuxQQDownload.js")
+res = requests.get("https://im.qq.com/rainbow/linuxQQDownload")
 res.encoding = "utf-8"
 
-download_list_json = re.search(r"var params\s*=\s*(\{.*?\});", res.text, re.DOTALL).group(1)
+download_list_json = re.search(
+    r"var params\s*=\s*(\{.*?\});", res.text, re.DOTALL
+).group(1)
 download_list = json.loads(download_list_json)
 
 version = download_list["version"]
