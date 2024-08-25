ff9f23353b96def952e18a6e0b22d999b6d2eb14
wcbing <cbing2002@outlook.com>
2024-08-25 12:44:58 +0800

    get google-chrome-stable info from official repo

--- get/google-chrome-stable.py
+++ get/google-chrome-stable.py
@@ -1,12 +1,12 @@
 import requests
-import json
+import re
 from module.check import deb
 
-version_url = "https://versionhistory.googleapis.com/v1/chrome/platforms/linux/channels/stable/versions"
-res = requests.get(version_url)
-version = json.loads(res.text)["versions"][0]["version"]
-# print(version)
+deb_repo = "https://dl.google.com/linux/chrome/deb/"
 
-x64_deb_url = "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+x64_deb_list = requests.get(deb_repo + "dists/stable/main/binary-amd64/Packages").text
+
+version = re.findall("-stable\nVersion: (.+)", x64_deb_list)[0]
+x64_deb_url = deb_repo + re.findall("pool/.+-stable_.+.deb", x64_deb_list)[0]
 
 deb("google-chrome-stable", version, x64_deb_url)
