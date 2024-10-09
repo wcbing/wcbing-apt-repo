0c4d6a99087e2b0c920007092c5d143700aa7b97
wcbing <cbing2002@outlook.com>
2024-10-09 21:30:49 +0800

    rename variables

--- get/github.py
+++ get/github.py
@@ -23,9 +23,9 @@ for i in github_info_list:
     repo = i["repo"]
     release_url = f"https://github.com/{repo}/releases/"
     latest_req = requests.head(release_url + "latest")
-    vversion = latest_req.headers["Location"].split("/")[-1]  # v1.1.1
-    version = vversion[1:]  # 1.1.1
-    x64_deb_name = i["x64_deb_name"].format(vversion=vversion, version=version)
+    version_tag = latest_req.headers["Location"].split("/")[-1]  # v1.1.1
+    stripped_version = version_tag[1:]  # 1.1.1
+    x64_deb_name = i["x64_deb_name"].format(version_tag=version_tag, stripped_version=stripped_version)
     x64_deb_url = release_url + "download/" + x64_deb_name
 
-    deb(i["name"], vversion, x64_deb_url)
+    deb(i["name"], version_tag, x64_deb_url)
