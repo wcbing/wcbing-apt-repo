d0c2de02f20d796ae38a1d7d5f62f4dc12a96637
wcbing <cbing2002@outlook.com>
2024-11-18 19:30:36 +0800

    update sunloginclient

--- get/sunloginclient.sh
+++ get/sunloginclient.sh
@@ -3,4 +3,4 @@ JSON=$(curl -fs "https://client-webapi.oray.com/softwares/SUNLOGIN_X_LINUX?x64=1
 VERSION=$(echo "$JSON" | jq -r '.versionno')
 X64_URL=$(echo $JSON | jq -r '.downloadurl')
 
-./check_downloader.py sunlogin $VERSION $X64_URL
+./check_downloader.py sunloginclient $VERSION $X64_URL
