575da81986e8bd6716c4cf3ca08d9a03f90e9271
shenmo <47873776+shenmo7192@users.noreply.github.com>
2024-11-22 00:26:42 +0800

    fix: Debian version should not contain V

--- get/bytedance-feishu-stable.sh
+++ get/bytedance-feishu-stable.sh
@@ -2,7 +2,7 @@ X64_JSON=$(curl -s "https://www.feishu.cn/api/package_info?platform=10")
 # arm64 https://www.feishu.cn/api/package_info?platform=12
 
 # "version_number": "Linux-x64-deb@V7.22.9"
-VERSION=$(echo $X64_JSON | jq -r ".data.version_number" | cut -d '@' -f 2)
+VERSION=$(echo $X64_JSON | jq -r ".data.version_number" | cut -d 'V' -f 2)
 X64_URL=$(echo $X64_JSON | jq -r ".data.download_link")
 
 ./check_downloader.py bytedance-feishu-stable $VERSION $X64_URL
