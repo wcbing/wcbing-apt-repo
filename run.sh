e084230a8b9f6cd2e75cc7a98d863824c7708c73
wcbing <cbing2002@outlook.com>
2024-12-02 16:03:37 +0800

    fix: tmpPackages path

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ $HOME/go/bin/github-downloader -r -o deb
 find get -maxdepth 1 -type f -name "*.sh" -exec sh {} \;
 cd deb
 # generate the local Packages
-apt-ftparchive packages . > tempPackages
+apt-ftparchive packages . > tmpPackages
 cd ..
 
-sed -i "s/\.\//\.\.\//g" deb/tmpPackages
+sed -i "s/\.\/http/\.\.\/http/g" deb/tmpPackages
 # merge the Packages files of third-party repositories
 ./merge-apt-repo.py --local deb/tmpPackages
 
