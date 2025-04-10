375316d7fc3b2e509e95fe49d5c3cf2c9b7aac22
wcbing <cbing2002@outlook.com>
2025-04-10 23:17:29 +0800

    update com.alibabainc.dingtalk

--- get/com.alibabainc.dingtalk.sh
+++ get/com.alibabainc.dingtalk.sh
@@ -1,4 +1,7 @@
 X64_URL=$(curl -sw %{redirect_url} https://www.dingtalk.com/win/d/qd=linux_amd64)
-VERSION=$(echo $X64_URL | cut -d '/' -f 8| cut -d '_' -f 2)
-
+VERSION=$(echo $X64_URL | sed 's#.*/##g' | cut -d '_' -f 2)
 ./check_downloader.py com.alibabainc.dingtalk $VERSION $X64_URL
+
+ARM64_URL=$(curl -sw %{redirect_url} https://www.dingtalk.com/win/d/qd=linux_arm64)
+VERSION=$(echo $ARM64_URL | sed 's#.*/##g' | cut -d '_' -f 2)
+./check_downloader.py com.alibabainc.dingtalk $VERSION $ARM64_URL arm64
