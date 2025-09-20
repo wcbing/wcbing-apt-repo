89ef8d2602a240e2977a9451631468df74c8887d
wcbing <cbing2002@outlook.com>
2025-09-20 22:10:07 +0800

    update linuxqq

--- get/linuxqq.sh
+++ get/linuxqq.sh
@@ -1,8 +1,12 @@
-JSON=$(curl -s https://im.qq.com/rainbow/linuxQQDownload | sed -nE 's/.*var params\s*=\s*(\{.*?\});.*/\1/p')
+# 正式版参考 https://github.com/flathub/com.qq.QQ/blob/master/com.qq.QQ.yaml
+JSON=$(curl -s https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/linuxConfig.js | sed -nE 's/.*var params\s*=\s*(\{.*?\});.*/\1/p')
 VERSION=$(echo $JSON | jq -r '.version + "_" + .updateDate')
 
-X64_URL=$(echo $JSON | jq -r '.x64DownloadUrl.deb')
-./check_downloader.py linuxqq $VERSION $X64_URL
+# 测试版参考 https://bbs.deepin.org/post/279737?offset=1&limit=20&postId=1658175
+
+# AMD64
+AMD64_URL=$(echo $JSON | jq -r '.x64DownloadUrl.deb')
+./check_downloader.py linuxqq $VERSION $AMD64_URL amd64
 
 # ARM64
 ARM64_URL=$(echo $JSON | jq -r '.armDownloadUrl.deb')
