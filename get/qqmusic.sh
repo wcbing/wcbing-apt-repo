f79f82db36e5655028922000296de44f66cb9fd8
wcbing <cbing2002@outlook.com>
2025-09-26 10:22:47 +0800

    update qqmusic

--- get/qqmusic.sh
+++ get/qqmusic.sh
@@ -1,6 +1,6 @@
 WEB_CONTENT=$(curl -s "https://y.qq.com/download/download.html")
 
 VERSION=$(echo $WEB_CONTENT | grep -o "Linux <span class=\"product_list__version\">最新版:[0-9\.]*" | cut -d ':' -f 2)
-AMD64_URL=$(echo $WEB_CONTENT | grep -o "https://[0-9a-z/\._]*amd64\.deb" | head -n 1)
+AMD64_URL=$(echo $WEB_CONTENT | grep -o "https://[^\"]*amd64\.deb[^\"]*" | head -n 1)
 
 ./check_downloader.py qqmusic $VERSION $AMD64_URL amd64
