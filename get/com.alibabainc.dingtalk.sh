611e165f5d9b2d7218365be28b148b8ab65a299f
wcbing <cbing2002@outlook.com>
2025-09-26 10:09:37 +0800

    fix: limit the number of parameters

--- get/com.alibabainc.dingtalk.sh
+++ get/com.alibabainc.dingtalk.sh
@@ -1,6 +1,6 @@
-X64_URL=$(curl -sw %{redirect_url} https://www.dingtalk.com/win/d/qd=linux_amd64)
-VERSION=$(echo $X64_URL | sed 's#.*/##g' | cut -d '_' -f 2)
-./check_downloader.py com.alibabainc.dingtalk $VERSION $X64_URL
+AMD64_URL=$(curl -sw %{redirect_url} https://www.dingtalk.com/win/d/qd=linux_amd64)
+VERSION=$(echo $AMD64_URL | sed 's#.*/##g' | cut -d '_' -f 2)
+./check_downloader.py com.alibabainc.dingtalk $VERSION $AMD64_URL amd64
 
 ARM64_URL=$(curl -sw %{redirect_url} https://www.dingtalk.com/win/d/qd=linux_arm64)
 VERSION=$(echo $ARM64_URL | sed 's#.*/##g' | cut -d '_' -f 2)
--- get/wechat.sh
+++ get/wechat.sh
@@ -1,13 +1,13 @@
 WEB_CONTENT=$(curl -s "https://linux.weixin.qq.com/")
 
-# X64
-X64_URL=$(echo $WEB_CONTENT | grep -o 'https:[0-9a-zA-Z/\._]*x86_64\.deb')
-Last_Modified=$(curl -sI $X64_URL | grep "Last-Modified")
+# AMD64
+AMD64_URL=$(echo $WEB_CONTENT | grep -o 'https:[0-9a-zA-Z/\._]*x86_64\.deb')
+Last_Modified=$(curl -sI $AMD64_URL | grep "Last-Modified")
 # Last-Modified: Wed, 06 Nov 2024 02:08:50 GMT
 VERSION=$(echo $Last_Modified | cut -d ' ' -f 3-6 | sed 's/ /-/g')
 # 06-Nov-2024-02:08:50
 
-./check_downloader.py wechat $VERSION $X64_URL
+./check_downloader.py wechat $VERSION $AMD64_URL amd64
 
 # ARM64
 ARM64_URL=$(echo $WEB_CONTENT | grep -o 'https:[0-9a-zA-Z/\._]*arm64\.deb')
