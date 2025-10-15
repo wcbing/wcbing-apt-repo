493206345a01e2083a7b8db6471d5014e06907de
wcbing <cbing2002@outlook.com>
2025-10-15 10:28:50 +0800

    fix sunloginclient

--- get/sunloginclient.sh
+++ get/sunloginclient.sh
@@ -1,6 +1,6 @@
 JSON=$(curl -fs "https://client-webapi.oray.com/softwares/SUNLOGIN_X_LINUX?x64=1")
 
-VERSION=$(echo "$JSON" | jq -r '.versionno')
-AMD64_URL=$(echo $JSON | jq -r '.downloadurl')
+VERSION=$(printf "%s" "$JSON" | jq -r '.versionno')
+AMD64_URL=$(printf "%s" "$JSON" | jq -r '.downloadurl')
 
 ./check_downloader.py sunloginclient $VERSION $AMD64_URL amd64
