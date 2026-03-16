1e1dcc4e53e5a25a26082e2c2ff2db4a16cc2ec6
wcbing <cbing2002@outlook.com>
2026-03-16 16:35:13 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ find get -type f -name "*.sh" -exec sh {} \;
 
 # generate the Packages file
 ## merge the Packages file from local package
-cat $(find packages -name "*.package") >> deb/tmpPackages
+cat $(find packages -name "*.package") > deb/tmpPackages
 
 ## merge the Packages files from third-party repositories
 ./merge-apt-repo.py --local deb/tmpPackages
@@ -13,5 +13,4 @@ cat $(find packages -name "*.package") >> deb/tmpPackages
 # generate the Release file
 cd deb/dists/wcbing && \
 apt-ftparchive release -c apt-ftparchive.conf . > Release && \
-gpg --yes --detach-sign -a -o Release.gpg Release && \
 gpg --yes --clearsign -o InRelease Release
