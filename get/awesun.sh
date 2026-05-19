436f45be342d9a97f9f796d626cafc255db50477
wcbing <cbing2002@outlook.com>
2026-05-19 22:45:15 +0800

    update awesun

--- get/awesun.sh
+++ get/awesun.sh
@@ -1,6 +1,6 @@
 JSON=$(curl -fs "https://client-webapi.oray.com/softwares/SUNLOGIN_X_LINUX?x64=1")
 
 VERSION=$(printf "%s" "$JSON" | jq -r '.versionno')
-AMD64_URL=$(printf "%s" "$JSON" | jq -r '.downloadurl')
+AMD64_URL=$(printf "%s" "$JSON" | jq -r '.downloadurlmultiple[0].url')
 
 ./check_downloader.py awesun $VERSION $AMD64_URL amd64
