fb4951b3be17442770743f795f7d3d32926f945a
wcbing <cbing2002@outlook.com>
2026-09-06 23:47:02 +0800

    update tdappdesktop

--- get/tdappdesktop.sh
+++ get/tdappdesktop.sh
@@ -1,10 +1,9 @@
 # 参考 https://github.com/SpecterShell/Dumplings/blob/main/Tasks/Tencent.TencentDocs/Script.ps1
 
-VERSION=$(curl -fs https://docs.qq.com/api/packageupgrade/update_manual | sed 's/.*"version\\\": \\\"\([0-9.]*\).*/\1/')
-BASE_URL="https://desktop.docs.qq.com/update/release/$VERSION/"
+VERSION=$(curl -fs https://docs.qq.com/api/packageupgrade/update_manual | jq -r '.result.update_info' | jq -r '.version')
 
-AMD64_URL="$BASE_URL"$(curl -fs "$BASE_URL"latest-linux.yml | grep '^path' | cut -d ' ' -f 2)
+AMD64_URL="https://docs.qq.com/api/package/get?channel_id=30001&version_id=latest&package_name=TencentDocs-x64.deb"
 ./check_downloader.py tdappdesktop $VERSION $AMD64_URL amd64
 
-ARM64_URL="$BASE_URL"$(curl -fs "$BASE_URL"latest-linux-arm64.yml | grep '^path' | cut -d ' ' -f 2)
+ARM64_URL="https://docs.qq.com/api/package/get?channel_id=30001&version_id=latest&package_name=TencentDocs-arm64.deb"
 ./check_downloader.py tdappdesktop $VERSION $ARM64_URL arm64
