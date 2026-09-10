4e60ffc9c8492babad119514b2f7b9db2c905fbe
Zax Zhao <2318494274@qq.com>
2026-09-10 11:49:40 +0800

    Update todesk.sh

--- get/todesk.sh
+++ get/todesk.sh
@@ -4,6 +4,12 @@ AMD64_URL=$(curl -s https://www.todesk.com/linux.html \
   | head -n 1)
 # AMD64_URL=https:/ /dl.todesk.com/linux/todesk-v4.7.2.0-amd64.deb
 
+ARM64_URL=$(curl -s https://www.todesk.com/linux.html \
+  | sed 's/\\u002F/\//g' \
+  | grep -oE 'https://dl\.todesk\.com/linux/todesk-v[0-9.]+-arm64\.deb' \
+  | head -n 1)
+# ARM64_URL=https://dl.todesk.com/linux/todesk-v4.8.5.1-arm64.deb
+
 VERSION=$(echo $AMD64_URL | cut -d '-' -f 2)
 
 ./check_downloader.py todesk $VERSION $AMD64_URL amd64
