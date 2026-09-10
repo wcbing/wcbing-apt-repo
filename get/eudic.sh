4d78b6e48aba088f844b6514cd86c97ab087f507
Zax Zhao <2318494274@qq.com>
2026-09-10 16:06:16 +0800

    Refactor eudic.sh to improve URL extraction

--- get/eudic.sh
+++ get/eudic.sh
@@ -1,8 +1,7 @@
-AMD64_URL=$(curl -s https://www.eudic.net/v4/en/app/download | grep -o 'https://[^"]*\.deb[^"]*')
-tmp=$(mktemp)
-curl -sL -o "$tmp" "$AMD64_URL"
-REAL_VERSION=$(dpkg-deb -f "$tmp" Version)
-rm -f "$tmp"
-echo "$REAL_VERSION"   # 应该是 13.5.2(20260910)
+WEB_CONTENT=$(curl -s https://www.eudic.net/v4/en/app/download)
 
-./check_downloader.py eudic "$REAL_VERSION" "$AMD64_URL" amd64
+AMD64_URL=$(echo $WEB_CONTENT | grep -o 'https://[^"]*\.deb[^"]*')
+# https://www.eudic.net/download/eudic.deb?v=2025-08-25
+VERSION=$(echo $AMD64_URL | cut -d '=' -f 2)
+
+./check_downloader.py eudic "$VERSION" "$AMD64_URL" amd64
