0cd6a640a36b8d386b4d5d2316a3a5a2eff753b5
Zax Zhao <2318494274@qq.com>
2026-09-10 11:47:54 +0800

    Update URL extraction method in todesk.sh

--- get/todesk.sh
+++ get/todesk.sh
@@ -1,4 +1,7 @@
-AMD64_URL=$(curl -s https://www.todesk.com/linux.html | grep -o "https://[0-9a-zA-Z_\/\.\-]*.deb" | head -n 1)
+AMD64_URL=$(curl -s https://www.todesk.com/linux.html \
+  | sed 's/\\u002F/\//g' \
+  | grep -oE 'https://dl\.todesk\.com/linux/todesk-v[0-9.]+-amd64\.deb' \
+  | head -n 1)
 # AMD64_URL=https:/ /dl.todesk.com/linux/todesk-v4.7.2.0-amd64.deb
 
 VERSION=$(echo $AMD64_URL | cut -d '-' -f 2)
