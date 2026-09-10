dbb06f4c478bd6a087a044db0ce9a85c8c34268e
Zax Zhao <2318494274@qq.com>
2026-09-10 12:27:32 +0800

    Refactor eudic.sh to fetch real version from .deb
    Updated script to extract the real version of Eudic from the downloaded .deb file instead of parsing the URL.

--- get/eudic.sh
+++ get/eudic.sh
@@ -1,7 +1,8 @@
-WEB_CONTENT=$(curl -s https://www.eudic.net/v4/en/app/download)
+AMD64_URL=$(curl -s https://www.eudic.net/v4/en/app/download | grep -o 'https://[^"]*\.deb[^"]*')
+tmp=$(mktemp)
+curl -sL -o "$tmp" "$AMD64_URL"
+REAL_VERSION=$(dpkg-deb -f "$tmp" Version)
+rm -f "$tmp"
+echo "$REAL_VERSION"   # 应该是 13.5.2(20260910)
 
-AMD64_URL=$(echo $WEB_CONTENT | grep -o 'https://[^"]*\.deb[^"]*')
-# https://www.eudic.net/download/eudic.deb?v=2025-08-25
-VERSION=$(echo $AMD64_URL | cut -d '=' -f 2)
-
-./check_downloader.py eudic "$VERSION" "$AMD64_URL" amd64
+./check_downloader.py eudic "$REAL_VERSION" "$AMD64_URL" amd64
